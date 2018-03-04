eb88098b67e2fdfd9a15f71672bfb763f9c738bd
Bharadwaj1431996 <bharadwajdaya@gmail.com>
2018-03-04 12:18:53 +0530

    three more calculators
    Math Calculators

--- math/math_calc.js
+++ math/math_calc.js
@@ -1,12 +1,34 @@
 $("[purpose=math_fact]").click(function(){
 	window.location.href="factorial.html";
 });
+$("[purpose=squareroot]").click(function(){
+	window.location.href="squareroot.html";
+});
+$("[purpose=cuberoot]").click(function(){
+	window.location.href="cuberoot.html";
+});
 var val=0;
 
 $("[purpose=fact_n]").on("change paste keyup", function() {
    val=$(this).val(); 
    factorial();
 });
+$("[purpose=squareroot_n]").on("change paste keyup", function() {
+   val=$(this).val(); 
+   squareroot();
+});
+$("[purpose=cuberoot_n]").on("change paste keyup", function() {
+   val=$(this).val(); 
+   cuberoot();
+});
+function cuberoot()
+{
+	$("[purpose=cuberoot_val]").val(Math.cbrt(val).toFixed(4));
+}
+function squareroot()
+{
+	$("[purpose=squareroot_val]").val(Math.sqrt(val).toFixed(4));
+}
 function factorial()
 {
 	var fact=1;
@@ -17,3 +39,4 @@ function factorial()
 	}
 	$("[purpose=fact_val]").val(fact);
 }
+
